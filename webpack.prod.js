3d84c354e84205c7e7b71de7139274c18cf6a691
inn3rGames <hi@inn3r.com>
2022-06-17 17:42:44 +0300

    Add production mode

---
+++ webpack.prod.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    devtool: "eval-cheap-source-map",
+    entry: "./src/game.ts",
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: {
+        host: "0.0.0.0",
+        port: 8080,
+        static: path.join(__dirname, "public"),
+        hot: true,
+        devMiddleware: {
+            publicPath: "/",
+        },
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|glb)$/i,
+                type: "asset/resource",
+            }
+        ],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "./public/index.html",
+            favicon: "./src/assets/favicon.ico",
+        })
+    ],
+};
