e35a38073a4c58f2562ce134e3a39da3384139a4
inn3rGames <hi@inn3r.com>
2022-08-19 19:36:42 +0300

    Update World.ts

--- src/World.ts
+++ src/World.ts
@@ -108,8 +108,8 @@ export default class World {
         this._scene.clearColor = Color4.FromHexString("#000000");
         //this._scene.autoClear = false;
         //this._scene.autoClearDepthAndStencil = false;
-        this._scene.blockMaterialDirtyMechanism = true;
-        this._scene.skipPointerMovePicking = true;
+        //this._scene.blockMaterialDirtyMechanism = true;
+        //this._scene.skipPointerMovePicking = true;
         //this._scene.freezeActiveMeshes(true);
     }
 
