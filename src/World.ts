df6cf7054717336041cc7d9f3bb0f1e7cb5b1d78
inn3rGames <hi@inn3r.com>
2022-09-03 23:54:07 +0300

    Anti-aliasing

--- src/World.ts
+++ src/World.ts
@@ -104,7 +104,7 @@ export default class World {
 
         const log = console.log;
         console.log = () => {};
-        this._engine = new Engine(this._canvas, true, undefined, true);
+        this._engine = new Engine(this._canvas, true);
         console.log = log;
 
         this._scene = new Scene(this._engine);
