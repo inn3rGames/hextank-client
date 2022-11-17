c5dba3d2427dc73ef6c8267bfa67584e41fafb29
inn3rGames <hi@inn3r.com>
2022-11-17 14:51:23 +0200

    Update version text

--- src/game.ts
+++ src/game.ts
@@ -5,8 +5,10 @@ import pkg from "../package.json";
 const world = new World();
 
 async function loadGame() {
+    const packageName = pkg.name;
+    const packageVersion = pkg.version;
     const versionDiv = document.getElementById("version");
-    versionDiv!.textContent = pkg.version;
+    versionDiv!.textContent = `${packageName} version: ${packageVersion}`;
 
     await world.loadWorld();
 
