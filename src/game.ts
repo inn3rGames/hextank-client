1db83a9e3cd6c6cb694633f45850061e694a6845
inn3rGames <hi@inn3r.com>
2022-11-17 13:18:42 +0200

    Display version

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,13 @@
 import World from "./World";
 import "./assets/styles/style.css";
+import pkg from "../package.json";
 
 const world = new World();
 
 async function loadGame() {
+    const versionDiv = document.getElementById("version");
+    versionDiv!.textContent = pkg.version;
+
     await world.loadWorld();
 
     world.createWorldMap();
