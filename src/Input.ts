64a15fe7b1d7265edc45767f31fad09d7111b94c
inn3rGames <hi@inn3r.com>
2022-11-20 22:14:46 +0200

    Fix input field

--- src/Input.ts
+++ src/Input.ts
@@ -425,32 +425,6 @@ export default class Input {
             }
         });
 
-        /* window.addEventListener("mousedown", (event) => {
-            event.preventDefault();
-        }); */
-        window.addEventListener("mousemove", (event) => {
-            event.preventDefault();
-        });
-        window.addEventListener("mouseup", (event) => {
-            event.preventDefault();
-        });
-        window.addEventListener("mouseleave", (event) => {
-            event.preventDefault();
-        });
-
-        /* window.addEventListener("touchstart", (event) => {
-            event.preventDefault();
-        }); */
-        window.addEventListener("toucmove", (event) => {
-            event.preventDefault();
-        });
-        window.addEventListener("touchend", (event) => {
-            event.preventDefault();
-        });
-        window.addEventListener("touchcancel", (event) => {
-            event.preventDefault();
-        });
-
         window.addEventListener("contextmenu", (event) => {
             event.preventDefault();
         });
