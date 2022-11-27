1074184c7a197ad66fa0d8f79551a2111af8f16c
inn3rGames <hi@inn3r.com>
2022-11-27 05:08:32 +0200

    Improve ux

--- src/Input.ts
+++ src/Input.ts
@@ -128,7 +128,7 @@ export default class Input {
             buttonUp.style.width = "13.5vmin";
             buttonUp.style.height = "15vmin";
             buttonUp.style.left = "10vmin";
-            buttonUp.style.bottom = "calc(1vmin + 15vmin + 1vmin)";
+            buttonUp.style.bottom = "calc(5vmin + 15vmin + 1vmin)";
             buttonUp.style.backgroundColor = upColor;
             buttonUp.style.borderRadius = "7.5vmin 7.5vmin 2.25vmin 2.25vmin";
 
@@ -180,7 +180,7 @@ export default class Input {
             buttonDown.style.width = "13.5vmin";
             buttonDown.style.height = "15vmin";
             buttonDown.style.left = "10vmin";
-            buttonDown.style.bottom = "1vmin";
+            buttonDown.style.bottom = "5vmin";
             buttonDown.style.backgroundColor = upColor;
             buttonDown.style.borderRadius = "2.25vmin 2.25vmin 7.5vmin 7.5vmin";
 
@@ -232,7 +232,7 @@ export default class Input {
             buttonLeft.style.width = "15vmin";
             buttonLeft.style.height = "13.5vmin";
             buttonLeft.style.right = "calc(10vmin + 15vmin + 1vmin)";
-            buttonLeft.style.bottom = "1vmin";
+            buttonLeft.style.bottom = "5vmin";
             buttonLeft.style.backgroundColor = upColor;
             buttonLeft.style.borderRadius = "7.5vmin 2.25vmin 2.25vmin 7.5vmin";
 
@@ -284,7 +284,7 @@ export default class Input {
             buttonRight.style.width = "15vmin";
             buttonRight.style.height = "13.5vmin";
             buttonRight.style.right = "10vmin";
-            buttonRight.style.bottom = "1vmin";
+            buttonRight.style.bottom = "5vmin";
             buttonRight.style.backgroundColor = upColor;
             buttonRight.style.borderRadius = "2.25vmin 7.5vmin 7.5vmin 2.25vmin";
 
@@ -339,7 +339,7 @@ export default class Input {
             buttonShoot.style.width = "15vmin";
             buttonShoot.style.height = "15vmin";
             buttonShoot.style.right = "calc(10vmin + 7.5vmin + 0.75vmin)";
-            buttonShoot.style.bottom = "calc(15.5vmin)";
+            buttonShoot.style.bottom = "calc(19.5vmin)";
             buttonShoot.style.backgroundColor = shootUpColor;
             buttonShoot.style.borderRadius = "7.5vmin 7.5vmin 7.5vmin 7.5vmin";
 
