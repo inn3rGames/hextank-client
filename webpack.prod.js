898f4133b08e2e411cdeceb660c2beade1c5e03b
inn3rGames <hi@inn3r.com>
2023-01-09 21:06:05 +0200

    Remove comments from build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,13 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const TerserPlugin = require("terser-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
+    optimization: {
+        minimizer: [new TerserPlugin({ extractComments: false })],
+    },
     plugins: [
         new CopyPlugin({
             patterns: [
